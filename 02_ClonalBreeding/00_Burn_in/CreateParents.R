fd03fac5294a8a90b96f0e8c1366c023c34eb3f2
jorgemasgomez <jorgemasz@gmail.com>
2024-11-27 12:47:01 +0100

    Update AlmondSim 27112024

---
+++ 02_ClonalBreeding/00_Burn_in/CreateParents.R
@@ -0,0 +1,57 @@
+# Create founders
+
+# # Create founder population
+# #founderPop = runMacs2(nInd     = nParents,
+#                       nChr     = nChr,
+#                       segSites = nQtl + nSnp,
+#                       genLen   = genLen,
+#                       mutRate  = mutRate)
+# 
+# # Set simulation parameters
+
+
+# Poner aqui la importacion de los parentales
+
+#La posicion genetica hacerla funcion de la posici?n f?sica (recomb rate del chr  x posici?n fisica)
+
+
+#Import physical map
+
+genMap_test <- scan("../Haplotypes/Chr_1_position.txt")
+recombination_rate_test<- 1.97e-08
+# Convert to genetic map
+genMap_test<- genMap_test*recombination_rate_test
+genMap=list(genMap_test)
+
+#Import gmatrix for haplotypes
+data <- scan("../Haplotypes/Chr_1_gmatrix.txt")
+chr_test <- matrix(data, ncol = 40, byrow = TRUE)
+chr_test<-t(chr_test)
+haplotypes = list(chr_test)
+
+founderPop = newMapPop(genMap=genMap, haplotypes=haplotypes)
+
+SP = SimParam$new(founderPop)
+
+
+# Add SNP chip
+SP$restrSegSites(nQtl,nSnp)
+if (nSnp > 0) {
+  SP$addSnpChip(nSnp)
+}
+
+# Add traits: trait represents yield
+SP$addTraitADG(nQtlPerChr = nQtl,
+               mean       = initMeanG,
+               var        = initVarG,
+               varGxE     = initVarGE)
+
+# Collect pedigree
+SP$setTrackPed(TRUE)
+
+# Create founder parents
+Parents = newPop(founderPop)
+
+# Set a phenotype to founder parents
+Parents = setPheno(Parents, varE = VarE, reps = repECT)
+rm(founderPop)
